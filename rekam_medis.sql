627b95be6d7f8a104fcbb893819894b034d2d013
dekapramesta <dekapramesta77@gmail.com>
2022-07-02 14:08:15 +0700

    db

--- rekam_medis.sql
+++ rekam_medis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Jun 2022 pada 14.00
+-- Waktu pembuatan: 02 Jul 2022 pada 09.07
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 8.0.2
 
@@ -41,11 +41,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (2, '2022_05_10_191044_create_t_user_table', 1),
 (3, '2022_05_10_191738_add_status_user_to_t_user_table', 1),
-(4, '2022_05_11_163639_create_t_pasien', 1),
-(5, '2022_05_13_140256_create_obats_table', 1),
-(6, '2022_05_14_115311_create_polikliniks_table', 1),
-(7, '2022_05_14_124539_create_t_dokter', 1),
-(8, '2022_05_15_120530_create_rekam_medis_table', 1);
+(4, '2022_05_13_140256_create_obats_table', 1),
+(5, '2022_05_14_115311_create_polikliniks_table', 1),
+(6, '2022_05_14_124539_create_t_dokter', 1),
+(7, '2022_05_14_124549_create_t_pasien', 1),
+(8, '2022_05_15_120530_create_rekam_medis_table', 1),
+(9, '2022_06_24_161923_create_poli_spesifiks_table', 1);
 
 -- --------------------------------------------------------
 
@@ -61,6 +62,18 @@ CREATE TABLE `personal_access_tokens` (
   `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `poli_spesifiks`
+--
+
+CREATE TABLE `poli_spesifiks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -88,9 +101,8 @@ CREATE TABLE `t_dokter` (
 --
 
 INSERT INTO `t_dokter` (`id`, `nama_dokter`, `spesialis`, `no_telp`, `alamat`, `id_poli`, `id_user`, `created_at`, `updated_at`) VALUES
-(1, 'Akaza uchihaaa', 'jantung', '09898922', 'Jogodayuh', 1, 3, '2022-06-04 08:13:57', '2022-06-05 00:49:47'),
-(2, 'John doe', 'kepala', '08921626', 'madiun', 1, 4, '2022-06-04 08:13:57', '2022-06-04 08:13:57'),
-(3722, 'deka pramesta', 'Gusi', '09889347928', 'Jogodayuh', 2, 8, '2022-06-04 14:44:54', '2022-06-04 14:44:54');
+(1, 'Stephan de Vries', 'jantung', '0989898', 'dagangan', 1, 3, '2022-06-27 12:00:59', '2022-06-27 12:00:59'),
+(2, 'John doe', 'kepala', '08921626', 'madiun', 1, 4, '2022-06-27 12:00:59', '2022-06-27 12:00:59');
 
 -- --------------------------------------------------------
 
@@ -112,7 +124,7 @@ CREATE TABLE `t_obat` (
 --
 
 INSERT INTO `t_obat` (`id`, `nama_obat`, `harga`, `keterangan`, `created_at`, `updated_at`) VALUES
-(1, 'Paramex', '7000', 'Obat Pusing Kepala', '2022-06-04 08:13:57', '2022-06-04 08:13:57');
+(1, 'Paramex', '7000', 'Obat Pusing Kepala', '2022-06-27 12:00:59', '2022-06-27 12:00:59');
 
 -- --------------------------------------------------------
 
@@ -142,7 +154,10 @@ CREATE TABLE `t_pasien` (
 --
 
 INSERT INTO `t_pasien` (`id_pasien`, `no_rm`, `nama_pasien`, `gender`, `nik`, `no_telp`, `alamat`, `ttl`, `pekerjaan`, `pendidikan`, `status`, `agama`, `created_at`, `updated_at`) VALUES
-(1, 'RM002192', 'mamad', 'Pria', '1212918', '0867237236', 'nglanduk', 'madiun 12-november-2000', 'mahasiswa', 'sma', 'Menikah', 'islam', '2022-06-04 08:13:57', '2022-06-04 08:13:57');
+(1, 'RM002192', 'mamad', 'Pria', '1212918', '0867237236', 'nglanduk', 'madiun 12-november-2000', 'mahasiswa', 'sma', 'Menikah', 'islam', '2022-06-27 12:00:59', '2022-06-27 12:00:59'),
+(2, 'RM002193', 'itachi', 'Pria', '35190482837823', '09889347928', 'Dagangan', 'madiun 12-november-2000', 'mahasiswa', 'S1', 'Single', 'Islam', '2022-06-27 12:03:25', '2022-06-27 12:03:25'),
+(3, 'RM002194', 'sveltess', 'Wanita', '198298192', '7247384632', 'Jogodayuh', 'Sarangan 12-November-2000', 'Pengacara', 'hjjkj', 'Menikah', 'Budha', '2022-06-27 12:05:11', '2022-06-27 12:05:11'),
+(4, 'RM002195', 'Joni', 'Pria', '35190407128', '0892162716', 'Dagangan', 'Sarangan 12-November-2000', 'mahasiswa', 'S1', 'Single', 'Budha', '2022-07-02 06:45:26', '2022-07-02 06:45:26');
 
 -- --------------------------------------------------------
 
@@ -163,8 +178,8 @@ CREATE TABLE `t_poliklinik` (
 --
 
 INSERT INTO `t_poliklinik` (`id`, `nama_poliklinik`, `gedung`, `created_at`, `updated_at`) VALUES
-(1, 'Poli anak', 'Lt 3', '2022-06-04 08:13:57', '2022-06-04 08:13:57'),
-(2, 'Poli GIgi', 'lt 2', '2022-06-04 13:38:40', '2022-06-04 13:38:40');
+(1, 'Poli Umum', 'Lt 1', '2022-06-27 12:00:59', '2022-06-27 12:00:59'),
+(2, 'Poli Gigi', 'Lt 1', '2022-06-27 12:00:59', '2022-06-27 12:00:59');
 
 -- --------------------------------------------------------
 
@@ -175,13 +190,13 @@ INSERT INTO `t_poliklinik` (`id`, `nama_poliklinik`, `gedung`, `created_at`, `up
 CREATE TABLE `t_rekammedis` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `id_pasien` bigint(20) UNSIGNED NOT NULL,
-  `keluhan` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `diagnosa` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tindakan` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `resep_obat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keluhan` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `diagnosa` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tindakan` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `resep_obat` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `id_dokter` bigint(20) UNSIGNED NOT NULL,
   `id_poli` bigint(20) UNSIGNED NOT NULL,
-  `tgl_periksa` date NOT NULL,
+  `tgl_periksa` date DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -191,7 +206,11 @@ CREATE TABLE `t_rekammedis` (
 --
 
 INSERT INTO `t_rekammedis` (`id`, `id_pasien`, `keluhan`, `diagnosa`, `tindakan`, `resep_obat`, `id_dokter`, `id_poli`, `tgl_periksa`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Panas', 'migrain', 'diperiksa', 'Paracetamol', 1, 1, '2022-05-17', '2022-06-04 08:13:57', '2022-06-04 08:13:57');
+(1, 1, 'Panas', 'migrain', 'diperiksa', 'Paracetamol', 1, 1, '2022-05-17', '2022-06-27 12:01:00', '2022-06-27 12:01:00'),
+(2, 2, 'Pusinh maszee', 'Cantengen', 'diperiksa', 'Salonpas', 2, 1, '2022-06-27', '2022-06-27 12:03:25', '2022-06-27 12:04:12'),
+(3, 3, NULL, NULL, NULL, NULL, 1, 2, NULL, '2022-06-27 12:05:11', '2022-06-27 12:05:11'),
+(4, 4, 'Pusinh maszee', 'Cantengen', 'diperiksa', 'Paracetamol', 2, 1, '2022-07-02', '2022-07-02 06:45:26', '2022-07-02 06:45:59'),
+(5, 2, NULL, NULL, NULL, NULL, 2, 2, NULL, '2022-07-02 06:46:55', '2022-07-02 06:46:55');
 
 -- --------------------------------------------------------
 
@@ -215,11 +234,11 @@ CREATE TABLE `t_user` (
 --
 
 INSERT INTO `t_user` (`id`, `username`, `password`, `foto`, `level`, `created_at`, `updated_at`, `status_user`) VALUES
-(1, 'deka', '$2y$10$OApBulHs1JgIsO8kPN.b0.fBH9dNXBTfiBkpxuXkXJVBaU3nE7jDC', '4443.jpg', 1, '2022-06-04 08:13:57', '2022-06-04 13:04:22', 1),
-(2, 'admin', '$2y$10$uRjMyOnAjjsypR7Mi1LPXOoPNrtJTu4WjLcZSLKHXFv7ehrE2/88y', '6332.jpg', 2, '2022-06-04 08:13:57', '2022-06-04 12:50:14', 1),
-(3, 'akaza', '$2y$10$cntZeukNlWV7ZoLmM50YZ.HEXbzBeoha6fx5mOrDf/kemlr2UrhWq', '1373.jpg', 3, '2022-06-04 08:13:57', '2022-06-04 13:07:10', 1),
-(4, 'patrick', '$2y$10$X4kx715dZBvONWwV3bxzbOaKZjR7f.DamD8o9QAPQyJpsPAMLxgzu', NULL, 3, '2022-06-04 08:13:57', '2022-06-04 08:13:57', 1),
-(8, 'pramesta', '$2y$10$ODdcXg6PFxprCsedXIzKhOxktB3g/lv6jGtCsw8PfQmKjnoOurrQi', NULL, 3, '2022-06-04 14:44:54', '2022-06-04 14:44:54', 1);
+(1, 'user1', '$2y$10$qm8WMnTmW05JdiWR9/9wkeNTwZIJk2ZyQDi5pez84CcrrSI45lik6', NULL, 1, '2022-06-27 12:00:59', '2022-07-02 06:40:53', 1),
+(2, 'admin', '$2y$10$uR2xfuV2ntba99iVwB.0/ecw7wB0r96.2nnvfLXg90vFqqwa80LLe', NULL, 2, '2022-06-27 12:00:59', '2022-06-27 12:00:59', 1),
+(3, 'dokter1', '$2y$10$9Rj7xoBvmn8YtKeor9xs5O5/4y2rdKoM1tZ7DGbzQz7.rU1khzpLy', NULL, 3, '2022-06-27 12:00:59', '2022-07-02 06:39:59', 1),
+(4, 'dokter2', '$2y$10$YlASVJADVP9bgIS8Ztn.zuNQp7dpYVjkul6W60mLt0hqTU6mDQ2Ia', NULL, 3, '2022-06-27 12:00:59', '2022-07-02 06:40:07', 1),
+(128781, 'kepalarm', '$2y$10$Um4uGmc7NoxVoX4IsEsv0ugI4vi9yPW5T1ht0DwiRfeiJE6MkCf0e', NULL, 4, '2022-07-02 06:59:32', '2022-07-02 07:05:43', 1);
 
 --
 -- Indexes for dumped tables
@@ -238,6 +257,12 @@ ALTER TABLE `personal_access_tokens`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `poli_spesifiks`
+--
+ALTER TABLE `poli_spesifiks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `t_dokter`
@@ -289,12 +314,18 @@ ALTER TABLE `t_user`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT untuk tabel `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `poli_spesifiks`
+--
+ALTER TABLE `poli_spesifiks`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -307,7 +338,7 @@ ALTER TABLE `t_obat`
 -- AUTO_INCREMENT untuk tabel `t_pasien`
 --
 ALTER TABLE `t_pasien`
-  MODIFY `id_pasien` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pasien` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `t_poliklinik`
@@ -319,13 +350,13 @@ ALTER TABLE `t_poliklinik`
 -- AUTO_INCREMENT untuk tabel `t_rekammedis`
 --
 ALTER TABLE `t_rekammedis`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128782;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
